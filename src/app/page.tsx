2e17cbd9f13289ed475e0fed7d5c5ed018c00569
Maxime Ancelin <ancelin.maxime@gmail.com>
2025-07-10 15:58:05 +0200

    Revert "export as static page"
    This reverts commit b8b852ada13371af80b000ae08b408437c35b5ed.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,16 @@
-"use client";
+import Image from "next/image";
 
 export default function Home() {
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <img
+        <Image
           className="dark:invert"
           src="/next.svg"
           alt="Next.js logo"
           width={180}
           height={38}
+          priority
         />
         <ol className="list-inside list-decimal text-sm/6 text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
           <li className="mb-2 tracking-[-.01em]">
@@ -31,7 +32,7 @@ export default function Home() {
             target="_blank"
             rel="noopener noreferrer"
           >
-            <img
+            <Image
               className="dark:invert"
               src="/vercel.svg"
               alt="Vercel logomark"
@@ -57,7 +58,7 @@ export default function Home() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          <img
+          <Image
             aria-hidden
             src="/file.svg"
             alt="File icon"
@@ -72,7 +73,7 @@ export default function Home() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          <img
+          <Image
             aria-hidden
             src="/window.svg"
             alt="Window icon"
@@ -87,7 +88,7 @@ export default function Home() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          <img
+          <Image
             aria-hidden
             src="/globe.svg"
             alt="Globe icon"
